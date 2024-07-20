a0ce254d8a4d44fefb408ddc7ee6e46ce6fe46c6
Pulsaaar <anatoliykuragin.litra@proton.me>
2024-07-20 15:02:39 +0300

    Rename

---
+++ WeatherApp/src/components/weather.jsx
@@ -0,0 +1,35 @@
+import { Card } from 'antd';
+
+function WeatherCard(props){
+    const {info, count} = props
+
+    if (!info.current) {
+        return <div>Loading...</div>
+    }
+
+    return (
+        <div>
+            <Card className='bg-slate-200' bordered={false}
+                title={
+                <div className='flex justify-center items-center'>
+                    <span>{info.location.name}</span>
+                </div>}
+    
+                style={{
+                    width: 300,
+                }}>
+                <p>
+                    <div className='flex items-center'> 
+                        {info.current.condition.text}
+                        <img src={`http:${info.current.condition.icon}`}/>
+                    </div>  
+                </p>
+                <p>Temperature: {info.current.temp_c} °C</p>
+                <p>Feels: {info.current.feelslike_c} °C</p>
+                <p>How many: {count}</p>
+            </Card>
+        </div>
+    )
+}
+
+export default WeatherCard;
